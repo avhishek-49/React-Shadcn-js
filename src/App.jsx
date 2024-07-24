cf1596014e73ed54ac669ddc4b190b882350d66e
Abishek Paudel <abishek.paudel@swifttech.com.np>
2024-07-24 09:59:09 +0545

    shadcn ui init in javascript with import of button and carousel comonents

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+import {CarouselDemo} from "./demo/carosuol";
+
+
+function App() {
+  return (
+    <>
+    <div className="px-20">
+    <Button>Hello abishek</Button>
+    </div>
+
+    <div><CarouselDemo/></div>
+    </>
+  )
+}
+
+export default App
